e1472f95600fac5076ad34efad474036bed74705
cemeguy <watthikorn.saka@bumail.net>
2024-03-04 16:10:45 +0700

    contol

--- Assets/AirplaneMovement.cs
+++ Assets/AirplaneMovement.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AirplaneMovement : MonoBehaviour
 {
-    public float enginePowerThrust, liftBooster, drag, angularDrag;
+    public float enginePowerThrust, liftBooster, drag, angularDrag,force;
 
     public Rigidbody rb;
 
@@ -12,34 +13,56 @@ public class AirplaneMovement : MonoBehaviour
     {
         // 1. เพิ่มแรงขับเคลื่อน
 
-        if (Input.GetKey(KeyCode.Space))
+        /* if (Input.GetKey(KeyCode.Space))
+         {
+             rb.AddForce(transform.forward * enginePowerThrust);
+         }
+ 
+         // 2. แรงยก
+ 
+         Vector3 lift = Vector3.Project(rb.velocity, transform.forward);
+         rb.AddForce(transform.up * lift.magnitude * liftBooster);
+ 
+         // 3. แรงต้าน
+ 
+         rb.drag = rb.velocity.magnitude * drag;
+         rb.angularDrag = rb.velocity.magnitude * angularDrag;
+ 
+         // 4. ควบคุม
+ 
+         // 4.1 ซ้าย/ขวา
+ 
+         rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1);
+ 
+         // 4.2 ขึ้น/ลง
+ 
+         rb.AddTorque(Input.GetAxis("Vertical") * transform.right);*/
+        if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(transform.forward * enginePowerThrust);
+            rb.AddForce(Vector3.forward * force);
+
         }
 
-        // 2. แรงยก
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * force);
+        }
 
-        Vector3 lift = Vector3.Project(rb.velocity, transform.forward);
-        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * force);
+            
+        }
 
-        // 3. แรงต้าน
-
-        rb.drag = rb.velocity.magnitude * drag;
-        rb.angularDrag = rb.velocity.magnitude * angularDrag;
-
-        // 4. ควบคุม
-
-        // 4.1 ซ้าย/ขวา
-
-        rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1);
-
-        // 4.2 ขึ้น/ลง
-
-        rb.AddTorque(Input.GetAxis("Vertical") * transform.right);
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * force);
+        }
     }
-        
     
-    
-    
+
+
+
+
 }
 
